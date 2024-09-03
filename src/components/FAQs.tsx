66bb9efe2fd8292b13ec7f762158fd71a3005988
unknown <david.patino1@utp.edu.co>
2024-09-03 17:36:00 -0500

    LogoTicket & Features

--- src/components/FAQs.tsx
+++ src/components/FAQs.tsx
@@ -1,3 +1,9 @@
+"use client"
+import { useState } from 'react';
+import clsx from 'clsx';
+import PlusIcon from '../assets/icons/plus.svg';
+import MinusIcon from '../assets/icons/minus.svg';
+
 const items = [
   {
     question: "What payment methods do you accept?",
@@ -21,6 +27,51 @@ const items = [
   },
 ];
 
+const AccordionItem = ({ 
+  question, 
+  answer,
+}: { 
+  question: string; 
+  answer: string;
+} ) => {
+    const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div 
+      className='py-7 border-b border-white/30' 
+      onClick={() => setIsOpen(!isOpen)}
+    >
+
+      <div className='flex items-center cursor-pointer'>
+        <span className='flex-1 text-lg font-bold'>{question}</span>
+        {isOpen ? <MinusIcon /> : <PlusIcon />}
+      </div>
+
+      <p className={clsx("mt-4", { 
+        hidden: !isOpen,
+        "": isOpen === true
+        })}>
+          {answer}
+        </p>
+    
+    </div>
+  )
+}
+
 export const FAQs = () => {
-  return null;
+  return (
+    <div className="bg-black text-white bg-gradient-to-b from-[#5D2CA8] to-black py-[72px] sm:py-24">
+      <div className="container">
+        
+        <h2 className='text-center text-5xl sm:text-6xl sm:max-w-[648px] mx-auto font-bold tracking-tighter'>Frequently asked questions</h2>
+        
+        <div className='mt-12 max-w-[648px] mx-auto'>
+          {items.map(({ question, answer }, index) => (
+            <AccordionItem key={index} question={question} answer={answer} />
+            ))
+          }
+        </div>
+      
+      </div>
+    </div>
+  )
 };
--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import acmeLogo from "../assets/images/acme.png";
 import quantumLogo from "../assets/images/quantum.png";
 import echoLogo from "../assets/images/echo.png";
@@ -15,5 +16,27 @@ const images = [
 ];
 
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="bg-black text-white py-[72px] sm:py-24">
+      <div className="container">
+        <h2 className="text-xl text-center text-white/70">
+          Trusted by the world’s most innovative teams
+        </h2>
+          <div className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative   after:right-0 before:left-0 before:top-0 after:top-0 before:bg-gradient-to-r before:from-black before:to-transparent after:bg-gradient-to-l after:from-black after:to-transparent ">
+            <div className="flex gap-16">
+              {images.map((image, index) => (
+                <Image
+                  key={index}
+                  src={image.src}
+                  alt={image.alt}
+                  width={200}
+                  height={200}
+                  className="flex-none h-8 w-auto"
+                />
+              ))}
+            </div>
+        </div>
+      </div>
+    </div>
+  )
 };
